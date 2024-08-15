c6642e588d111c85368343d4d0cec2b663425385
ebenezer <39166121+cookiesikeri@users.noreply.github.com>
2024-08-15 23:56:43 +0100

    Update app.js

--- AppTest/app.js
+++ AppTest/app.js
@@ -31,7 +31,7 @@ mongoose
   console.log('Database is connected');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const server = app.listen(port, () => {
 console.log(`App running on ${port}`);
 });
